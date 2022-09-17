bf776feec3af095cd075accb2764a0190821e472
Aabhash Rana <aabhashbikramrana@gmail.com>
2022-09-16 19:45:10 -0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+
+import React, { useState } from "react";
+import MovieList from "./components/MovieList";
+
+const App = () => {
+  const [movies, setMovies] = useState([
+    {
+      "Title": "Star Wars",
+      "Year": "1977",
+      "imdbID": "tt0076759",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BNzg4MjQxNTQtZmI5My00YjMwLWJlMjUtMmJlY2U2ZWFlNzY1XkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg"
+  },
+  {
+      "Title": "Star Wars: Episode V - The Empire Strikes Back",
+      "Year": "1980",
+      "imdbID": "tt0080684",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
+  },
+  ]);
+  return <div>
+    <MovieList movies = {movies}/>
+    </div>
+
+};
+
+
+
+
+
+
+
+
+export default App;
+
+
